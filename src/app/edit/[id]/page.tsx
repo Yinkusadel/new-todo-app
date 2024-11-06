82b78ed699db27667fe1e35a121d88d88e1289ce
olayinka <adeleyeolayinka73@gmail.com>
2024-11-06 03:39:08 +0100

    add todos

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,23 +1,13 @@
-
 import Todo from "@/app/components/Todo"
 import fetchTodo from "@/lib/fetchTodo"
 import { notFound } from "next/navigation"
 
 export const revalidate = 0
 
-type Props = {
-    params: {
-        id: string
-    }
-}
-
-export default async function page({ params: { id } }: Props) {
-
-    const todo = await fetchTodo(id)
+export default async function page({ params }: { params: { id: string } }) {
+    const todo = await fetchTodo(params.id)
 
     if (!todo) notFound()
 
-    return (
-        <Todo {...todo} />
-    )
+    return <Todo {...todo} />
 }
